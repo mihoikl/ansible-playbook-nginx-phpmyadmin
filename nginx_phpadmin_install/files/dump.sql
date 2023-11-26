07295c1de3976f5cf4397b47373d8bb3b09e12c0
mihoikl <theuselesslad2@gmail.com>
2023-11-26 11:31:28 +0400

    commit

---
+++ nginx_phpadmin_install/files/dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (           
+  message varchar(255) NOT NULL         
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
+  INSERT INTO test(message) VALUES('welcome to my github');
+  INSERT INTO test(message) VALUES('made by');         
+  INSERT INTO test(message) VALUES('teichrib')
